a44a147b25bae133b15da60b1abbac5b99efe82b
Janaina Tavares <112178884+profJana@users.noreply.github.com>
2023-08-30 20:18:39 -0300

    Add files via upload

---
+++ PRO-V2-C25-Solucao-main/js/Board.js
@@ -0,0 +1,37 @@
+class Board {
+  constructor(x, y, width, height) {
+    //qual código deixa o options como estático verdadeiro
+    
+    // var options = {
+    //   isstatic: rue
+    // };
+
+    // var options = {
+    //   isStatic: true
+    // };
+
+    // var options
+    //   isStatic: true
+    // };
+
+    // var options = {
+    //   isStatic true
+    // };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/board.png");
+
+    World.add(world, this.body);
+  }
+
+  display() {
+    var pos = this.body.position;
+    push();
+    imageMode(CENTER);
+    image(this.image, pos.x, pos.y, this.width, this.height);
+    pop();
+  }
+}
